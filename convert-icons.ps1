c08249908ada908d88073a5c85ca1c539f59e4cb
blackbear007-maker <blackbear007-maker@users.noreply.github.com>
2026-07-03 15:12:17 +0800

    Redesign icon: F bars extend left, T bar extends right from center arrow shaft

--- convert-icons.ps1
+++ convert-icons.ps1
@@ -40,17 +40,17 @@ function Create-PngIcon {
     )
     $graphics.DrawLines($pen, $arrowHeadPoints)
 
-    # Draw F horizontal bars (extend through center to both sides)
+    # Draw F horizontal bars (extend left from center)
     $fTopY = [int]($Size * 0.35)
     $fMidY = [int]($Size * 0.47)
     $fLeftX = [int]($Size * 0.35)
-    $fRightX = [int]($Size * 0.65)
-    $graphics.DrawLine($pen, $fLeftX, $fTopY, $fRightX, $fTopY)  # F top bar (through center)
-    $graphics.DrawLine($pen, $fLeftX, $fMidY, $fRightX, $fMidY)  # F middle bar (through center)
+    $graphics.DrawLine($pen, $centerX, $fTopY, $fLeftX, $fTopY)  # F top bar (left from center)
+    $graphics.DrawLine($pen, $centerX, $fMidY, $fLeftX, $fMidY)  # F middle bar (left from center)
 
-    # Draw T horizontal bar (extend through center to both sides)
+    # Draw T horizontal bar (extend right from center)
     $tTopY = [int]($Size * 0.35)
-    $graphics.DrawLine($pen, $fLeftX, $tTopY, $fRightX, $tTopY)  # T top bar (through center)
+    $tRightX = [int]($Size * 0.65)
+    $graphics.DrawLine($pen, $centerX, $tTopY, $tRightX, $tTopY)  # T top bar (right from center)
 
     # Save as PNG
     $bitmap.Save($OutputPath, [System.Drawing.Imaging.ImageFormat]::Png)
